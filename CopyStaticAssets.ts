d166651d696a8a00922ff4226564b5e4509db5fe
Edgar Arakelyan <arakee1@unlv.nevada.edu>
2019-01-06 14:07:57 -0800

    Project structure initialized

---
+++ CopyStaticAssets.ts
@@ -0,0 +1,6 @@
+// Copy static assets from src to dist
+import * as shell from "shelljs";
+
+shell.cp("-R", "src/public/js/lib", "dist/public/js/");
+shell.cp("-R", "src/public/fonts", "dist/public/");
+shell.cp("-R", "src/public/images", "dist/public/");
